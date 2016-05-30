8b45daffdee3eebedbb3e4e02993e866086821ac
Chalice Stevens <chaliceariel@gmail.com>
2016-05-30 15:45:02 -0600

    excercise completed

---
+++ ajax.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+
+
+	$( "#subButton" ).on('click',function() {
+		$(".gifList").empty();
+
+		var input = $("#inputText").val().replace(" ", "+");
+		var apiKey = "&api_key=dc6zaTOxFJmzC";
+
+		$.ajax({
+			method: "GET",
+			url: 'http://api.giphy.com/v1/gifs/search?q=' + input + apiKey
+			// setup asyncronous promise  
+		}).then (function(response){ 
+
+			console.log(response); 
+
+			//do something with response here
+			var imageArray = [];
+			for (var i = 0; i < response.data.length; i++){
+				imageArray[i] = response.data[i].images.downsized_medium.url;
+			}
+
+			var div = $(".gifList");
+
+			$.each(imageArray, function(i, value) {
+        	$("<img />").attr("src", value).appendTo(div);
+   			
+   			});
+		});
+	});
+    
+});
